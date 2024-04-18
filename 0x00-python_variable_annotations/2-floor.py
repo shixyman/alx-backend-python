45f76b893a13e1bd17282699efbe45575e8f5187
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-04-18 15:02:34 +0100

    add files

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Contains a single function that calculates the floor of a number."""
+
+
+def floor(a: float) -> int:
+    '''Computes the floor of a floating-point number.
+    '''
+    return int(a)
