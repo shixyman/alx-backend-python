499fd36f3a7ad66562fba46b85e0cefa4667c17f
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-04-18 15:18:51 +0100

    add files

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Contains Augmented code with the correct duck-typed annotations."""
+from typing import Any, Sequence, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    '''Retrieves the first element of a sequence if it exists.
+    '''
+    if lst:
+        return lst[0]
+    else:
+        return None
+    
