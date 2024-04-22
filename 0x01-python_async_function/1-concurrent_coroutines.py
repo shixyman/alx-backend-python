dcc8943ef6005510f3fd3beb30bf53195e1b1a2e
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-04-22 14:48:15 +0100

    add files

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,19 +3,15 @@
 specified delay between each call."""
 import asyncio
 from typing import List
-
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    delays = []
-    tasks = []
-
-    for _ in range(n):
-        task = asyncio.create_task(wait_random(max_delay))
-        tasks.append(task)
-
-    for task in tasks:
-        delay = await task
-        delays.append(delay)
-
-    return sorted(delays)
+    """ Let's execute multiple coroutines at the same time with async  """
+    delays: List[float] = []
+    all_delays: List[float] = []
+    for i in range(n):
+        delays.append(wait_random(max_delay))
+    for delay in asyncio.as_completed(delays):
+        earliest_result = await delay
+        all_delays.append(earliest_result)
+    return all_delays
