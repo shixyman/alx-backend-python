c20735cc559387bca24a48e043ded6a4b8ff7b68
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-04-22 19:07:10 +0100

    fix

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,6 +5,7 @@ import asyncio
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """ Let's execute multiple coroutines at the same time with async """
     delays: List[float] = []
