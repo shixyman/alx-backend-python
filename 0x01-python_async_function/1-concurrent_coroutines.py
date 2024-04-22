13dde2950db83ba096d4992304b9c90a8dd9aa83
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-04-22 14:47:23 +0100

    add files

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Contains a method that spawns wait_random n times with a
+specified delay between each call."""
+import asyncio
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    delays = []
+    tasks = []
+
+    for _ in range(n):
+        task = asyncio.create_task(wait_random(max_delay))
+        tasks.append(task)
+
+    for task in tasks:
+        delay = await task
+        delays.append(delay)
+
+    return sorted(delays)
