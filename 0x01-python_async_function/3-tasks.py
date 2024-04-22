656639d5b63c872c03b8da276fc6623eefe17b0a
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-04-22 15:38:34 +0100

    add files

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -7,8 +7,11 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-# Import wait_random from the previous file
-from file_with_wait_random import wait_random
-
-def task_wait_random(max_delay: int) -> Task:
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+    Returns a task that waits for a random number of seconds
+    Args:
+        max_delay: maximum number of seconds that the task will wait
+    Returns: an asyncio.Task object
+    """
     return asyncio.create_task(wait_random(max_delay))
