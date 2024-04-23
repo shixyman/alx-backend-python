d31b1822a99634ebbfa9a304050c89e495c5ef41
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-04-23 13:26:03 +0100

    add task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 """Contains a method that spawns Tasks n times with a
 specified delay between each call."""
-import asyncio
-import random
 
-async def async_generator():
-    for _ in range(10):
-        await asyncio.sleep(1)  # Asynchronously wait for 1 second
-        yield random.randint(0, 10)  # Yield a random number between 0 and 10
+
+async def async_generator() -> Generator[float, None, None]:
+    """
+    async_generator - function to loop 10 times
+    Arguments:
+        no arguments
+    Returns:
+        nothing
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
